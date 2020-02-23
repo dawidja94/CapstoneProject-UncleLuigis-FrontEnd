d71651865fc3e18e646bcfaad983c3c01102bf6c
Tom O <thomasowca@yahoo.com>
2020-02-22 20:02:25 -0600

    something.

--- capstone-web-app/src/Components/Menu/Menu.tsx
+++ capstone-web-app/src/Components/Menu/Menu.tsx
@@ -39,50 +39,52 @@ export default class Menu extends React.Component<IMenuProps, IMenuState> {
                 <br />
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-1"></div>
-                        <div className="col-lg-10 col-sm-12 col-md-12">
-                            {this.state.foodItems.map((item, key) => {
-                                return (
-                                <div className="card-container">
-                                    <div className="card">
-                                        <div className="card-margin">
-                                            <div className="row">
-                                                <div className="col-lg-6 col-md-12 col-sm-12">
-                                                    <h4>{item.name}</h4>
-                                                    <div className="custom-card-image">
-                                                        <img className="card-img-top custom" src={`/Images/Food/${item.imageURL}`}></img>
+                        <div className="card">
+                            <div className="col-lg-12 col-sm-12 col-md-12 justify-content-center">
+                                <br />
+                                <h2 className="text-center menu-header">Food Menu</h2>
+                                {this.state.foodItems.map((item, key) => {
+                                    return (
+                                    <div className="card-container">
+                                        <div className="card">
+                                            <div className="card-margin">
+                                                <div className="row">
+                                                    <div className="col-lg-6 col-md-12 col-sm-12">
+                                                        <h4>{item.name}</h4>
+                                                        <div className="custom-card-image">
+                                                            <img className="card-img-top custom" src={`/Images/Food/${item.imageURL}`}></img>
+                                                        </div>
+                                                        <br className="menu-item-break"/>
                                                     </div>
-                                                    <br />
-                                                </div>
-                                                <div className="col-lg-6 col-md-12 col-sm-12">
-                                                    <div className="card-right-section">
-                                                        <br />
-                                                        <span><h6>Category: {item.type}</h6></span>
-                                                        <span><h6>Gluten Free: {item.glutenFree ? "Yes" : "No"}</h6></span>
-                                                        <span><h6>Vegan: {item.vegan ? "Yes" : "No"}</h6></span>
-                                                        <span><h6>Description: <i>{item.description}</i></h6></span>
-                                                        <span><h6>Price: ${item.price}</h6></span>
-                                                        <span>
-                                                            <h6>Quantity:</h6>
-                                                            <select className="form-control quantity-custom">
-                                                                <option></option>
-                                                                <option>1</option>
-                                                                <option>2</option>
-                                                                <option>3</option>
-                                                                <option>4</option>
-                                                                <option>5</option>
-                                                            </select>
-                                                        </span>
-                                                        <button className="btn btn-outline-danger">Add To Carry Out</button>
+                                                    <div className="col-lg-6 col-md-12 col-sm-12">
+                                                        <div className="card-right-section">
+                                                            <br />
+                                                            <span><h6>Category: {item.type}</h6></span>
+                                                            <span><h6>Gluten Free: {item.glutenFree ? "Yes" : "No"}</h6></span>
+                                                            <span><h6>Vegan: {item.vegan ? "Yes" : "No"}</h6></span>
+                                                            <span><h6>Description: <i>{item.description}</i></h6></span>
+                                                            <span><h6>Price: ${item.price}</h6></span>
+                                                            <span>
+                                                                <h6>Quantity:</h6>
+                                                                <select className="form-control quantity-custom">
+                                                                    <option></option>
+                                                                    <option>1</option>
+                                                                    <option>2</option>
+                                                                    <option>3</option>
+                                                                    <option>4</option>
+                                                                    <option>5</option>
+                                                                </select>
+                                                            </span>
+                                                            <button className="btn btn-outline-danger">Add To Carry Out</button>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>);
-                            })}
+                                    </div>);
+                                })}
+                            </div>
                         </div>
-                        <div className="col-lg-1"></div>
                     </div>
                 </div>
             </div>
