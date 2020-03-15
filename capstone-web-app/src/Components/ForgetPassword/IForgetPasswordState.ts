60a2f57d945a0f14733f3a020bb20227c6fcaaa0
dawidja94 <dawidjasionowski@lewisu.edu>
2020-03-15 12:36:23 -0500

    added to forget password

--- capstone-web-app/src/Components/ForgetPassword/IForgetPasswordState.ts
+++ capstone-web-app/src/Components/ForgetPassword/IForgetPasswordState.ts
@@ -1,4 +1,15 @@
 export default interface IForgetPasswordState {
-
+    userName: string,
+    email: string,
+    phoneNumber: string,
+    password: string,
+    confirmPassword: string,
+    foundCustomerId: number,
+    isFormValid: boolean,
+    showValidationModal: boolean,
+    emptyField: string,
+    passwordTooShortError: string;
+    passwordsNotMatching: string;
+    noMatchingAccount: string;
     
 }
