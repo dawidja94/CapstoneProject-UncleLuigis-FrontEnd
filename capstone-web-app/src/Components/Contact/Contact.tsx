94f4592a8fde52f8ce115b44d16c05f57a124f94
Tom O <thomasowca@yahoo.com>
2020-03-17 19:41:00 -0500

    Latest and final changes of 3/17.

--- capstone-web-app/src/Components/Contact/Contact.tsx
+++ capstone-web-app/src/Components/Contact/Contact.tsx
@@ -5,6 +5,8 @@ import Footer from "../Footer/Footer";
 import { Redirect } from "react-router-dom";
 import IContactState from "./IContactState";
 import IContactProps from "./IContactProps";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as icons from "@fortawesome/free-solid-svg-icons";
 
 export default class Contact extends React.Component<IContactProps, IContactState> {
     public constructor(props: any) {
@@ -18,15 +20,38 @@ export default class Contact extends React.Component<IContactProps, IContactStat
         return (
             <div>
                 <Navbar />
-                <br />
-                <br />
-                <br />
-                <br />
-                <div className="container-fluid">
-                    <div id="contactBackground">
-
+                <div id="contactBackground">
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <div className="container">
+                        <div className="card contact">
+                            <h1 className="font-weight-lighter custom text-center"><FontAwesomeIcon icon={icons.faPhoneAlt}/> Contact Us</h1>
+                            <hr />
+                            <div className="card-body">
+                                <h5 className="font-weight-bolder">Uncle Luigi's Bistro</h5>
+                                <h5 className="font-weight-bold">1 University Pkwy, Romeoville, IL 60446</h5>
+                                <h5 className="font-weight-bold">Phone: (815) 838-0500</h5>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                </div>   
+                <Footer />
             </div>
         );
     }
